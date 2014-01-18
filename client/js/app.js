704331111e696cb8029f0b38037f439aa329c09f
Daniel Huhn <daniel_huhn@outlook.com>
2014-01-18 22:01:28 +0100

    added slider to the interface prototype

--- client/js/app.js
+++ client/js/app.js
@@ -1,3 +1,21 @@
 $(function(){
-    $('.slider').slider();
+    
+    function updateColorResult(){
+        $('#colorResult').css('background',
+                                      'rgb('+sliderR.getValue()+','+
+                                             sliderG.getValue()+','+
+                                             sliderB.getValue()+')');
+    };
+    
+    var sliderR = $('input[data-slider-id="slider_R"]').slider()
+        .on('slide', updateColorResult)
+        .data('slider');
+    var sliderG = $('input[data-slider-id="slider_G"]').slider()
+        .on('slide', updateColorResult)
+        .data('slider');
+    var sliderB = $('input[data-slider-id="slider_B"]').slider()
+        .on('slide', updateColorResult)
+        .data('slider');
+    
+    updateColorResult();
 });
