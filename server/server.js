0de95e1549c17bc237243e526c1e867a3dc0fe89
Daniel Huhn <daniel_huhn@outlook.com>
2014-01-18 20:15:29 +0100

    added sliders and modenizer.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,11 @@ var app = express();
 
 app.use(express.json());
 
-app.use('/', express.static('client/'));
+app.use('/', express.static('client/html'));
+app.use('/css', express.static('client/css'));
+app.use('/js', express.static('client/js'));
+app.use('/img', express.static('client/img'));
+app.use('/fonts', express.static('client/fonts'));
 
 /*
 app.post('/', function(req, res){
