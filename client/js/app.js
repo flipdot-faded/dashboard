5586f6043841c83a5c85164bde2bc5716ae9e876
Daniel Huhn <daniel_huhn@outlook.com>
2014-01-18 23:16:11 +0100

    added bootstrap-switch which provides nice, touch enabled checkboxes

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,15 @@
 $(function(){
     
+    $('input[type=checkbox]').each(function(){
+        var self = $(this);
+        
+        self.attr("data-on", "success");
+        self.attr("data-on-label", "ein");
+        self.attr("data-off", "danger");
+        self.attr("data-off-label", "aus");
+        self.bootstrapSwitch();
+    });
+    
     function updateColorResult(){
         $('#colorResult').css('background',
                                       'rgb('+sliderR.getValue()+','+
